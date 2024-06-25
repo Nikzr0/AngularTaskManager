857d8f6eff961d619df6f8a9059d00c13395fb94
Nikzr0 <npepelov@gmail.com>
2024-06-25 15:57:27 +0300

    Creating the main structure and adding functionality for creating and completing tasks for each user.

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, computed,  input, Output, EventEmitter, output} from '@angular/core';
+import { type User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+}) 
+
+export class UserComponent {
+  // decoratro approach
+  @Input({required: true}) user! : User;
+  @Input({required: true}) selected!: boolean;
+  @Output() select = new EventEmitter<string>(); 
+  //select = output<string>(); // under the hood it is the same as the commented line above
+
+  imagePath = computed(() => {
+  return 'assets/users/' + this.user.avatar;
+  });
+
+  // No matter which approach you use, the program will work the same way and it is easy to switch
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+ 
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+  
+
+  onSelectUser(){
+    this.select.emit(this.user.id);
+  }
+}
